6dc27c6f5c54fe161b911b7a2a18bfe6bc0fc5f0
AndersSSWork <andersss.arbejde@gmail.com>
2019-08-24 19:21:34 +0100

    Preparing EventHandler for the ui
    I think that the EventHandler should just inject the closing of the menu into the button. That's being worked on right now, but now it should have everything we need to do that. Long message.

--- Assets/Scripts/Events/EventHandler.cs
+++ Assets/Scripts/Events/EventHandler.cs
@@ -7,10 +7,20 @@ public class EventHandler : MonoBehaviour
     [SerializeField]
     int _eventsToCall;
 
+    [SerializeField]
+    GameObject _uiPrefab;
 
+    private GameObject _currentUI; //Just here to null check
+
+    [SerializeField]
+    Transform _canvasTransform;
 
     public void CallEvent(TextContents toDisplay, int callerId = -1, bool ignoreCount = false)
     {
+        if(_currentUI == null)
+        {
+            _currentUI = GameObject.Instantiate(_uiPrefab, _canvasTransform);
+        }
         TextContentUIDisplay.SetDisplayText(toDisplay);
 
         if (!ignoreCount)
