dade85e07a3246a46f382b836f62f942cafd99be
AndersSSWork <andersss.arbejde@gmail.com>
2019-08-24 11:13:56 +0100

    Messed a bit with controls
    Messed around a bit with controlling a rigidbody. Not sure if this is how to proced.

---
+++ Assets/Scripts/CarMov.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMov : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody _rigidBody;
+
+    public void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        if(vertical != 0 || horizontal != 0)
+        {
+            _rigidBody.velocity = _rigidBody.velocity + -1* this.transform.right * vertical * speed + this.transform.forward * horizontal * speed;
+            Debug.Log(_rigidBody.velocity);
+        }
+    }
+}
