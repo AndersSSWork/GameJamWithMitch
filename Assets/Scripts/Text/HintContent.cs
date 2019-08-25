95473998b47aee11bfdd3ec5336482c6fe2ca178
Daniel Milo <danielmilo12@gmail.com>
2019-08-25 20:24:36 +0300

    Injectables - hint and multi

---
+++ Assets/Scripts/Text/HintContent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintContent : MonoBehaviour
+{
+    public string hintGiver;
+    public string hintContent;
+    public Color hintColor;
+}
