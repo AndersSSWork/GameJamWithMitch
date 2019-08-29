ce901ef0c985204dac5776c0181791688dcfb97b
Scribbles and Flapjacks <mpsmitch@gmail.com>
2019-08-29 03:30:48 -0500

    Added color and house fronts. Also scaled down the map and added assets for future npcs

---
+++ Assets/RandomHouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHouse : MonoBehaviour
+{
+      public GameObject HUD;
+     
+      void Start() {
+         
+         Texture2D[] textures = Resources.LoadAll<Texture2D>("Houses");
+         Texture2D texture = textures[Random.Range(0, textures.Length)];
+         HUD.GetComponent<Renderer>().material.mainTexture = texture;
+     }
+}
---
+++ Assets/Single_Face.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single_Face : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+	Mesh mesh = GetComponent<MeshFilter>().mesh;
+	Vector2[] uvSingleFace = { 
+		new Vector2(0, 1),
+		new Vector2(1, 1),
+		new Vector2(0, 0),
+		new Vector2(1, 0),
+
+	
+	
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+
+
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1),
+		new Vector2(-1, -1)
+	};
+	mesh.uv = uvSingleFace;
+    }
+}
