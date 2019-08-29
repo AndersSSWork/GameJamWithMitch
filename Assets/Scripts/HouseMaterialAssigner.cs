6d5642abaee291ccd350fb34d1cfa7046f6337e6
AndersSSWork <andersss.arbejde@gmail.com>
2019-08-29 11:10:47 +0100

    Houses colored again
    Now the houses have a script that just assigns the material. Dammit github.

---
+++ Assets/Scripts/HouseMaterialAssigner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseMaterialAssigner : MonoBehaviour
+{
+    public Material newMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer[] ToGiveMat = this.GetComponentsInChildren<MeshRenderer>();
+        if(ToGiveMat != null)
+        {
+            foreach(MeshRenderer SingleMesh in ToGiveMat)
+            {
+                SingleMesh.material = newMat;
+            }
+        }
+    }
+
+}
