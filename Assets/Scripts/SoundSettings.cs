3c7d7163e3c52c67b64c6e066f5b7e09bf357bce
AndersSSWork <andersss.arbejde@gmail.com>
2019-08-30 10:39:48 +0100

    Made a sample title screen with volume sliders.
    That's it.

---
+++ Assets/Scripts/SoundSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSettings : MonoBehaviour
+{
+    public Slider musicVolume;
+    public Slider crashVolume;
+
+    void Start()
+    {
+        _ChangeVolumes(1, 1);
+    }
+
+    public void ReadSliders()
+    {
+        _ChangeVolumes(musicVolume.value, crashVolume.value);
+    }
+
+    private void _ChangeVolumes(float music, float crash)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", music);
+        PlayerPrefs.SetFloat("CrashVolume", crash);
+    }
+}
