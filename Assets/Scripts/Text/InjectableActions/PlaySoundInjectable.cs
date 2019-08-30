aa20cc6760ace9e001849cfbb6d7da1bce2116e3
Daniel Milo <danielmilo12@gmail.com>
2019-08-30 18:08:48 +0300

    installed content and trees

---
+++ Assets/Scripts/Text/InjectableActions/PlaySoundInjectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * 
+ * DOES NOT WORK
+ * 
+ */
+[CreateAssetMenu(fileName = "NewPlaySoundInjectable", menuName = "ScriptableObjects/TextContentActions/PlaySoundInjectable", order = 3)]
+public class PlaySoundInjectable : InjectableResponseAction
+{
+    [SerializeField] AudioSource sound;
+
+    public override void ExecuteResponseAction(TextContents contents)
+    {
+        sound.PlayDelayed(0.4f);
+    }
+}
